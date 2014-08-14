3c236c88cf1e5d5a7ff79dfaded7ac32970c43ad
agustinsantosmendez <agustinsantosmendez@gmail.com>
2014-08-14 19:38:53 +0200

    Added new tutorial, Property changes

---
+++ DynamicProxyTutorial/DynamicProxyTutorial03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProxyTutorial03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This should generate an error. A NormalClass is not a IDDSObject
+            // This allows us to check an error condition by the compiler.
+            //var obj1 = SimpleEngine.MakeDDSObject<NormalClass>("ADomain");
+
+             // The object is intercepted when the app try to change a virtual field (property)
+            var obj2 = SimpleEngine.MakeDDSObject<SampleClass>("ADomain");
+
+            Console.WriteLine("The value of the object is = " + obj2.Value);
+            Console.WriteLine("I'll try to modify the value to 3");
+
+            obj2.Value = 3;
+
+            Console.WriteLine("The value of the object is now = " + obj2.Value);
+
+            Console.WriteLine("Press Enter to finish");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DynamicProxyTutorial/DynamicProxyTutorial03/SampleClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicProxyTutorial03
+{
+
+    public interface IDDSObject
+    {
+        string DomainName { get; set; }
+    }
+
+    /// <summary>
+    /// This class will be intercepted
+    /// </summary>
+    public class SampleClass : IDDSObject
+    {
+        /// <summary>
+        /// We want to check when the app change this value
+        /// and propagate this.
+        /// </summary>
+        public virtual int Value { get; set; }
+
+        public string DomainName { get; set; }
+    }
+
+    /// <summary>
+    /// This class can't be intercepted
+    /// </summary>
+    public class NormalClass
+    {
+        public virtual int Value { get; set; }
+    }
+}
