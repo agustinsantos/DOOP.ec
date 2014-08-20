f9af4b2cddcb2da2a66cc9750e1858d23678d258
Agustin Santos <agustinsantosmendez@gmail.com>
2014-08-20 13:44:23 +0200

    Put some comments

--- DDS-RTPS/RTPS-Impl/Rtps/Utils/Config/WellKnownPortsConfig.cs
+++ DDS-RTPS/RTPS-Impl/Rtps/Utils/Config/WellKnownPortsConfig.cs
@@ -11,49 +11,92 @@ namespace Doopec.Rtps.Config
     {
         private WellKnownPortsConfig() { }
 
-        [ConfigurationProperty("portBase", DefaultValue = "7400")]
+        /// <summary>
+        /// Port Base number. This number sets the starting
+        /// point for deriving port numbers used for Simple
+        /// Endpoint Discovery Protocol (SEDP). This property
+        /// is used in conjunction with DG, PG, D0 (or DX), and D1
+        /// to construct the necessary Endpoints for RTPS
+        /// discovery communication. (see section 9.6.1.1 in the
+        /// OMG DDS-RTPS specification in how these
+        /// Endpoints are constructed)
+        /// </summary>
+        [ConfigurationProperty("portBase", DefaultValue = "7400", IsRequired = true)]
         public int PortBase
         {
             get { return (int)this["portBase"]; }
             set { this["portBase"] = value; }
         }
 
-        [ConfigurationProperty("domainIdGain", DefaultValue = "250")]
+        /// <summary>
+        /// An integer value representing the Domain Gain. This
+        /// is a multiplier that assists in formulating Multicast
+        /// or Unicast ports for RTPS.
+        /// </summary>
+        [ConfigurationProperty("domainIdGain", DefaultValue = "250", IsRequired = true)]
         public int DomainIdGain
         {
             get { return (int)this["domainIdGain"]; }
             set { this["domainIdGain"] = value; }
         }
 
-        [ConfigurationProperty("participantIdGain", DefaultValue = "2")]
+        /// <summary>
+        /// An integer that assists in configuring SPDP Unicast
+        /// ports and serves as an offset multiplier as
+        /// participants are assigned addresses using the
+        /// formula:
+        /// PB + DG * domainId + d1 + PG * participantId
+        /// (see section 9.6.1.1 in the OMG DDS-RTPS
+        /// specification in how these Endpoints are
+        /// constructed)
+        /// </summary>
+        [ConfigurationProperty("participantIdGain", DefaultValue = "2", IsRequired = true)]
         public int ParticipantIdGain
         {
             get { return (int)this["participantIdGain"]; }
             set { this["participantIdGain"] = value; }
         }
 
-        [ConfigurationProperty("offsetd0", DefaultValue = "0")]
+        /// <summary>
+        /// An integer value that assists in providing an offset
+        /// for calculating an assignable port in SPDP Multicast
+        /// configurations. The formula used is:
+        /// PB + DG * domainId + d0 
+        /// (see section 9.6.1.1 in the OMG DDS-RTPS
+        /// specification in how these Endpoints are
+        /// constructed)
+        /// </summary>
+        [ConfigurationProperty("offsetd0", DefaultValue = "0", IsRequired = true)]
         public int Offsetd0
         {
             get { return (int)this["offsetd0"]; }
             set { this["offsetd0"] = value; }
         }
 
-        [ConfigurationProperty("offsetd1", DefaultValue = "10")]
+        /// <summary>
+        /// An integer value that assists in providing an offset
+        /// for calculating an assignable port in SPDP Unicast
+        /// configurations. The formula used is:
+        /// PB + DG * domainId + d1 + PG * participantId
+        /// (see section 9.6.1.1 in the OMG DDS-RTPS
+        /// specification in how these Endpoints are
+        /// constructed)
+        /// </summary>
+        [ConfigurationProperty("offsetd1", DefaultValue = "10", IsRequired = true)]
         public int Offsetd1
         {
             get { return (int)this["offsetd1"]; }
             set { this["offsetd1"] = value; }
         }
 
-        [ConfigurationProperty("offsetd2", DefaultValue = "1")]
+        [ConfigurationProperty("offsetd2", DefaultValue = "1", IsRequired = true)]
         public int Offsetd2
         {
             get { return (int)this["offsetd2"]; }
             set { this["offsetd2"] = value; }
         }
 
-        [ConfigurationProperty("offsetd3", DefaultValue = "1")]
+        [ConfigurationProperty("offsetd3", DefaultValue = "11", IsRequired = true)]
         public int Offsetd3
         {
             get { return (int)this["offsetd3"]; }
