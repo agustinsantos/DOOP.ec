0922f0d52b3d5ea4b9f4ee7af8805dbed6be8d07
Agustin Santos <agustinsantosmendez@gmail.com>
2014-11-13 15:13:23 +0100

    added a comment

--- DDS-RTPS/DDS/omg/dds/core/Bootstrap.cs
+++ DDS-RTPS/DDS/omg/dds/core/Bootstrap.cs
@@ -1,20 +1,21 @@
-/* Copyright 2010, Object Management Group, Inc.
- * Copyright 2010, PrismTech, Inc.
- * Copyright 2010, Real-Time Innovations, Inc.
- * All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/// <copyright file="Bootstrap.cs" Company="Doop.ec">
+/// Copyright 2010, Object Management Group, Inc.
+/// Copyright 2010, PrismTech, Inc.
+/// Copyright 2010, Real-Time Innovations, Inc.
+/// All rights reserved.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///</copyright>
 
 
 
@@ -348,12 +349,16 @@ namespace org.omg.dds.core
 
             // --- Time & Duration: ----------------------------------------------
 
-            /**
-             * Construct a {@link Duration} of the given magnitude.
-             * 
-             * A duration of magnitude {@link Long#MAX_VALUE} indicates an
-             * infinite duration, regardless of the units specified.
-             */
+
+            /// <summary>
+            /// Construct a {@link Duration} of the given magnitude.
+            /// 
+            /// A duration of magnitude {@link Long#MAX_VALUE} indicates an
+            /// infinite duration, regardless of the units specified.
+            /// </summary>
+            /// <param name="duration"></param>
+            /// <param name="unit"></param>
+            /// <returns></returns>
             ModifiableDuration newDuration(long duration, TimeUnit unit);
 
             /**
