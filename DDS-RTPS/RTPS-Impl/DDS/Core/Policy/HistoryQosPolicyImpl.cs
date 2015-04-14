372823adf47f701c2b0e68e30c9acbbd0c12946c
andresrubiop <andresrubiop@msn.com>
2015-04-14 17:13:28 -0500

    Nueva Politica

---
+++ DDS-RTPS/RTPS-Impl/DDS/Core/Policy/HistoryQosPolicyImpl.cs
@@ -0,0 +1,46 @@
+﻿using org.omg.dds.core.policy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doopec.Dds.Core.Policy
+{
+    public class HistoryQosPolicyImpl : QosPolicy, HistoryQosPolicy
+    {
+        private readonly HistoryQosPolicyKind kind;
+        private readonly int getDepth;
+
+        public HistoryQosPolicyImpl(HistoryQosPolicyKind kind, int getDepth)
+        {
+            this.kind = kind;
+            this.getDepth = getDepth;
+        }
+
+        public HistoryQosPolicyKind GetKind()
+        {
+            return kind;
+        }
+
+        public int GetDepth()
+        {
+            return getDepth ;
+        }
+
+        public QosPolicyId GetId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public org.omg.dds.core.Bootstrap GetBootstrap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public org.omg.dds.core.policy.modifiable.ModifiableHistoryQosPolicy Modify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
