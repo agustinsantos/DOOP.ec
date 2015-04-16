1cea18690b1f750707edc5c811bd5a33cac16690
andresrubiop <andresrubiop@msn.com>
2015-04-16 12:32:22 -0500

    Recuperacion

---
+++ DDS-RTPS/RTPS-Impl/DDS/Core/Policy/DestinationOrderQosPolicyImpl.cs
@@ -0,0 +1,40 @@
+﻿using org.omg.dds.core.policy;
+using org.omg.dds.core.policy.modifiable;
+using org.omg.dds.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doopec.Dds.Core.Policy
+{
+    public class DestinationOrderQosPolicyImpl: QosPolicy, DestinationOrderQosPolicy
+    {
+        private readonly DestinationOrderQosPolicyKind kind;
+
+        public DestinationOrderQosPolicyImpl(DestinationOrderQosPolicyKind kind)
+        {
+            this.kind = kind;
+        }
+        public DestinationOrderQosPolicyKind GetKind()
+        {
+            return kind;
+        }
+
+        public QosPolicyId GetId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bootstrap GetBootstrap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ModifiableDestinationOrderQosPolicy Modify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DDS-RTPS/RTPS-Impl/DDS/Core/Policy/TransportPriorityQosPolicyImpl.cs
@@ -0,0 +1,38 @@
+﻿using org.omg.dds.core.policy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doopec.Dds.Core.Policy
+{
+    class TransportPriorityQosPolicyImpl : QosPolicy,TransportPriorityQosPolicy
+    {
+        private readonly int getValue;
+        public TransportPriorityQosPolicyImpl(int getValue)
+        {
+            this.getValue = getValue;
+        }
+
+        public int GetValue()
+        {
+            return getValue;
+        }
+
+        public QosPolicyId GetId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public org.omg.dds.core.Bootstrap GetBootstrap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public org.omg.dds.core.policy.modifiable.ModifiableTransportPriorityQosPolicy Modify()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
