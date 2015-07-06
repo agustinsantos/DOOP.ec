d032abb74b1e8b33fb8ef3cd7e725f9efab10609
{name} <{emailaddress}>
2015-07-06 11:50:50 -0500

    Unit Tests

--- DDS-RTPS/ConfigurationTests/TestDDSConfig.cs
+++ DDS-RTPS/ConfigurationTests/TestDDSConfig.cs
@@ -22,6 +22,10 @@ namespace ConfigurationTests
             Assert.IsNotNull(ddsConfig);
         }
 
+        /// <summary>
+        /// Check that DDS configuration has the correct values.
+        /// <DDS xmlns="urn:Configuration" vendor="Doopec" version="2.1">
+        /// </summary>
         [TestMethod]
         public void TestDDSConfigurationAttributes()
         {
@@ -32,7 +36,10 @@ namespace ConfigurationTests
             Assert.AreEqual("2.1", ddsConfig.Version);
             Assert.AreEqual(new Version(2, 1), Version.Parse(ddsConfig.Version));
         }
-
+        /// <summary>
+        /// 
+        /// <boostrapType name="default" type="Doopec.Dds.Core.BootstrapImpl, Doopec"/>
+        /// </summary>
         [TestMethod]
         public void TestboostrapType()
         {
@@ -41,7 +48,14 @@ namespace ConfigurationTests
             Assert.AreEqual("default", ddsConfig.BoostrapType.Name);
             Assert.AreEqual("Doopec.Dds.Core.BootstrapImpl, Doopec", ddsConfig.BoostrapType.Type);
          }
-
+        /// <summary>
+        /// Check that domain configuration is correct.
+        /// <domain name="Main" id="0">
+        /// <transportProfile name="defaultRtps"/>
+        /// <qoSProfile name="defaultQoS"/>
+        /// <guid kind="Fixed" val="7F294ABE-33F2-40B9-BFF5-7D9376EA061C"/>
+        /// </domain>
+        /// </summary>
         [TestMethod]
         public void TestExistDomains()
         {
@@ -49,7 +63,13 @@ namespace ConfigurationTests
             Assert.AreEqual("Main", ddsConfig.Domains[0].Name);
             Assert.AreEqual(0, ddsConfig.Domains[0].Id);
         }
-
+        /// <summary>
+        /// <domain name="Main" id="0">
+        /// <transportProfile name="defaultRtps"/>
+        /// <qoSProfile name="defaultQoS"/>
+        /// <guid kind="Fixed" val="7F294ABE-33F2-40B9-BFF5-7D9376EA061C"/>
+        /// </domain>
+        /// </summary>
         [TestMethod]
         public void TestExistQoSProfiles()
         {
@@ -59,7 +79,13 @@ namespace ConfigurationTests
             Assert.AreEqual("defaultQoS", ddsConfig.QoSProfiles["defaultQoS"].Name);
         }
 
-
+        /// <summary>
+        /// <domain name="Main" id="0">
+        /// <transportProfile name="defaultRtps"/>
+        /// <qoSProfile name="defaultQoS"/>
+        /// <guid kind="Fixed" val="7F294ABE-33F2-40B9-BFF5-7D9376EA061C"/>
+        /// </domain>
+        /// </summary>
         [TestMethod]
         public void TestExistTransport()
         {
@@ -67,7 +93,13 @@ namespace ConfigurationTests
             Assert.IsNotNull(domain.TransportProfile);
             Assert.AreEqual("defaultRtps", domain.TransportProfile.Name);
         }
-
+        /// <summary>
+        /// <domain name="Main" id="0">
+        /// <transportProfile name="defaultRtps"/>
+        /// <qoSProfile name="defaultQoS"/>
+        /// <guid kind="Fixed" val="7F294ABE-33F2-40B9-BFF5-7D9376EA061C"/>
+        /// </domain>
+        /// </summary>
         [TestMethod]
         public void TestExistQoSProfile()
         {
@@ -75,7 +107,13 @@ namespace ConfigurationTests
             Assert.IsNotNull(domain.QoSProfile);
             Assert.AreEqual("defaultQoS", domain.QoSProfile.Name);
         }
-
+        /// <summary>
+        /// <domain name="Main" id="0">
+        /// <transportProfile name="defaultRtps"/>
+        /// <qoSProfile name="defaultQoS"/>
+        /// <guid kind="Fixed" val="7F294ABE-33F2-40B9-BFF5-7D9376EA061C"/>
+        /// </domain>
+        /// </summary>
         [TestMethod]
         public void TestExistGuidKind()
         {
@@ -84,7 +122,13 @@ namespace ConfigurationTests
             Assert.AreEqual(Doopec.Configuration.GuiKind.Fixed, domain.Guid.Kind);
             Assert.AreEqual("7F294ABE-33F2-40B9-BFF5-7D9376EA061C", domain.Guid.Val);
         }
-
+        /// <summary>
+        /// <domain name="Main" id="0">
+        /// <transportProfile name="defaultRtps"/>
+        /// <qoSProfile name="defaultQoS"/>
+        /// <guid kind="Fixed" val="7F294ABE-33F2-40B9-BFF5-7D9376EA061C"/>
+        /// </domain>
+        /// </summary>
         [TestMethod]
         public void TestExistQoSProfilePolicy()
         {
@@ -94,7 +138,12 @@ namespace ConfigurationTests
             Assert.IsNotNull(qosProfile);
             Assert.AreEqual("defaultQoS", qosProfile.Name);
         }
-
+        /// <summary>
+        /// <qoSProfileDef name="defaultQoS">
+        /// <domainParticipantFactoryQos name="defaultDomainParticipantFactoryQoS">
+        /// <entityFactory autoenableCreatedEntities="true"/>
+        /// </domainParticipantFactoryQos>
+        /// </summary>
         [TestMethod]
         public void TestExistDomainParticipantFactoryQos()
         {
@@ -104,7 +153,12 @@ namespace ConfigurationTests
             Assert.IsNotNull(qosProfile.DomainParticipantFactoryQos);
             Assert.AreEqual("defaultDomainParticipantFactoryQoS", qosProfile.DomainParticipantFactoryQos.Name);
         }
-
+        /// <summary>
+        /// <domainParticipantQos name="defaultDomainParticipantQoS">
+        /// <entityFactory autoenableCreatedEntities="true"/>
+        /// <userData  value=""/>
+        /// </domainParticipantQos>
+        /// </summary>
         [TestMethod]
         public void TestExistDomainParticipantQos()
         {
@@ -114,7 +168,13 @@ namespace ConfigurationTests
             Assert.IsNotNull(qosProfile.DomainParticipantQos);
             Assert.AreEqual("defaultDomainParticipantQoS", qosProfile.DomainParticipantQos.Name);
         }
-
+        /// <summary>
+        /// <topicQoS name="defaultTopicQoS">
+        /// <topicData value=""/>
+        /// <deadline  period="100"/>
+        /// <durability  kind="VOLATILE"/>
+        /// </topicQoS>
+        /// </summary>
         [TestMethod]
         public void TestExistTopicQoS()
         {
@@ -124,7 +184,14 @@ namespace ConfigurationTests
             Assert.IsNotNull(qosProfile.TopicQoS);
             Assert.AreEqual("defaultTopicQoS", qosProfile.TopicQoS.Name);
         }
-
+        /// <summary>
+        /// <publisherQoS name="defaultPublisherQoS">
+        /// <entityFactory autoenableCreatedEntities="true"/>
+        /// <groupData value=""/>
+        /// <partition value=""/>
+        /// <presentation accessScope="INSTANCE" coherentAccess="true" orderedAccess="true"/>
+        /// </publisherQoS>
+        /// </summary>
         [TestMethod]
         public void TestExistPublisherQoS()
         {
@@ -134,7 +201,14 @@ namespace ConfigurationTests
             Assert.IsNotNull(qosProfile.PublisherQoS);
             Assert.AreEqual("defaultPublisherQoS", qosProfile.PublisherQoS.Name);
         }
-
+        /// <summary>
+        /// <subscriberQoS name="defaultSubscriberQoS">
+        /// <entityFactory autoenableCreatedEntities="true"/>
+        /// <groupData value=""/>
+        /// <partition value=""/>
+        /// <presentation accessScope="INSTANCE" coherentAccess="true" orderedAccess="true"/>
+        /// </subscriberQoS>
+        /// </summary>
         [TestMethod]
         public void TestExistSubscriberQoS()
         {
@@ -145,7 +219,25 @@ namespace ConfigurationTests
             Assert.AreEqual("defaultSubscriberQoS", qosProfile.SubscriberQoS.Name);
         }
 
-
+        /// <summary>
+        /// <dataWriterQoS name="defaultDataWriterQoS">
+        /// <deadline period="1"/>
+        /// <destinationOrder  kind="BY_SOURCE_TIMESTAMP"/>
+        /// <durability  kind="VOLATILE"/>
+        /// <durabilityService historyDepth="0" historyKind="KEEP_LAST" maxInstances="1" maxSamples="1" maxSamplesPerInstance="1" serviceCleanupDelay="100"/>
+        /// <history kind="KEEP_LAST" depth="1"/>
+        /// <latencyBudget duration="100"/>
+        /// <lifespan duration="100"/>
+        /// <liveliness kind="AUTOMATIC" leaseDuration="100"/>
+        /// <ownership kind="SHARED"/>
+        /// <ownershipStrength value="100"/>
+        /// <reliability kind="BEST_EFFORT" maxBlockingTime="1000"/>
+        /// <resourceLimits maxInstances="1" maxSamples="1" maxSamplesPerInstance="1"/>
+        /// <transportPriority value="1"/>
+        /// <userData value=""/>
+        /// <writerDataLifecycle autodisposeUnregisteredInstances="true"/>
+        /// </dataWriterQoS>
+        /// </summary>
         [TestMethod]
         public void TestExistDataWriterQoS()
         {
@@ -155,7 +247,22 @@ namespace ConfigurationTests
             Assert.IsNotNull(qosProfile.DataWriterQoS);
             Assert.AreEqual("defaultDataWriterQoS", qosProfile.DataWriterQoS.Name);
         }
-
+        /// <summary>
+        /// <dataReaderQoS name="defaultDataReaderQoS">
+        /// <deadline period="1"/>
+        /// <destinationOrder  kind="BY_SOURCE_TIMESTAMP"/>
+        /// <durability  kind="VOLATILE"/>
+        /// <history kind="KEEP_LAST" depth="1"/>
+        /// <latencyBudget duration="100"/>
+        /// <liveliness kind="AUTOMATIC" leaseDuration="100"/>
+        /// <ownership kind="SHARED"/>
+        /// <reliability kind="BEST_EFFORT" maxBlockingTime="1000"/>
+        /// <resourceLimits maxInstances="1" maxSamples="1" maxSamplesPerInstance="1"/>
+        /// <readerDataLifecycle autopurgeDisposedSamplesDelay="1000" autopurgeNowriterSamplesDelay="1000"/>
+        /// <timeBasedFilter minimumSeparation="1000"/>
+        /// <userData value=""/>
+        /// </dataReaderQoS>
+        /// </summary>
         [TestMethod]
         public void TestExistDataReaderQoS()
         {
--- DDS-RTPS/DDSTests/DomainParticipantTests.cs
+++ DDS-RTPS/DDSTests/DomainParticipantTests.cs
@@ -3,6 +3,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using org.omg.dds.domain;
 using org.omg.dds.core;
 using Doopec.Dds.Domain;
+using org.omg.dds.pub;
+using System.Collections.Generic;
+using Doopec.Dds.Pub;
 
 namespace DDSTests
 {
@@ -10,13 +13,18 @@ namespace DDSTests
     public class DomainParticipantTests
     {
         [TestMethod]
-        public void TestCreateDomainParticipant()
+        public void TestGetfactory()
         {
             DomainParticipantFactory factory = DomainParticipantFactory.GetInstance(Bootstrap.CreateInstance());
 
             Assert.IsNotNull(factory);
             Assert.AreEqual(typeof(Doopec.Dds.Domain.DomainParticipantFactoryImpl), factory.GetType());
+        }
 
+        [TestMethod]
+        public void TestCreateDomainParticipant()
+        {
+            DomainParticipantFactory factory = DomainParticipantFactory.GetInstance(Bootstrap.CreateInstance());
             DomainParticipantImpl dp = factory.CreateParticipant() as DomainParticipantImpl;
             Assert.IsNotNull(dp);
             Assert.AreEqual(0, dp.DomainId);
@@ -63,6 +71,66 @@ namespace DDSTests
             Assert.IsNotNull(dp2);
             Assert.AreEqual(0, dp2.DomainId);
             Assert.AreEqual(0, dp2.RtpsParticipantId);
+        }
+        /// <summary>
+        /// 2.2.2.2.1.1 create_publisher
+        /// This operation creates a Publisher with the desired QoS policies and attaches to it the specified PublisherListener.
+        /// </summary>
+        [TestMethod]
+        public void TestCreatePublisher01()
+        {
+            //PRECONDITIONS
+            DomainParticipantFactory factory = DomainParticipantFactory.GetInstance(Bootstrap.CreateInstance());
+            DomainParticipantImpl dp = factory.CreateParticipant() as DomainParticipantImpl;
+            Publisher publisher= dp.CreatePublisher();
+            //POSTCONDITIONS
+            Assert.IsNotNull(publisher);
+        }
+
+        /// <summary>
+        /// 2.2.2.2.1.1 create_publisher
+        /// This operation creates a Publisher with the desired QoS policies and attaches to it the specified PublisherListener.
+        /// </summary>
+        [TestMethod]
+        public void TestCreatePublisher02()
+        {
+            //PRECONDITIONS
+            DomainParticipantFactory factory = DomainParticipantFactory.GetInstance(Bootstrap.CreateInstance());
+            DomainParticipantImpl dp = factory.CreateParticipant() as DomainParticipantImpl;
+            PublisherQos qos=null;
+            PublisherListener listener=null;
+            ICollection<Type> statuses=null;
+            Publisher publisher=dp.CreatePublisher(qos,listener,statuses);
+            //POSTCONDITIONS
+            Assert.IsNotNull(publisher);
+        }
+        [TestMethod]
+        public void TestCreatePublisher03()
+        {
+            //PRECONDITIONS
+            DomainParticipantFactory factory = DomainParticipantFactory.GetInstance(Bootstrap.CreateInstance());
+            DomainParticipantImpl dp = factory.CreateParticipant() as DomainParticipantImpl;
+            PublisherQos qos = new PublisherQosImpl(factory.GetBootstrap());
+            PublisherListener listener = null;
+            ICollection<Type> statuses = new List<Type>();
+            Publisher publisher = dp.CreatePublisher(qos, listener, statuses);
+            //POSTCONDITIONS
+            Assert.IsNotNull(publisher);
+        }
+        [TestMethod]
+        public void TestCreatePublisher04()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 2.2.2.2.1.2 delete_publisher
+        /// This operation deletes an existing Publisher.
+        /// </summary>
+        [TestMethod]
+        public void TestDeletePublisher()
+        {
+
         }
     }
 }
